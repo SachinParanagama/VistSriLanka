6fc5a22317a3760db86eed1ca67a67320fd62d96
ShehaniHettiarachchi <88665593+ShehaniHettiarachchi@users.noreply.github.com>
2023-05-07 17:01:10 +0530

    changes

--- BACKEND/models/hotelRating.js
+++ BACKEND/models/hotelRating.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 
 const hotelRatingSchema = new Schema({
   hotel: {
-    type: String,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Hotel",
     required: true,
   },
   user: {
