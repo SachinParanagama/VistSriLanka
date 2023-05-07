e35e736f2e7dd81171b6d773011b3a769e832357
Niruni Imesha <niruniimesha@gmail.com>
2023-05-07 15:25:37 +0530

    insert package

--- frontend/src/containers/HotelManagement/AddPackages.js
+++ frontend/src/containers/HotelManagement/AddPackages.js
@@ -6,68 +6,53 @@ import './AddHotel.css'
 import '../../components/common/admin/HomeNavBar.css'
 import AdminNavBar from "../../components/common/admin/AdminNavBar";
 
-export default function AddHotel() {
+export default function AddPackage() {
     const clearData = () => {
         window.location.reload(false);
     };
 
-    return ( 
+return ( 
+    <div className="body">
+        <AdminNavBar/>
+        <AddHotelNavBar />            
 
-        <div className="body">
-            <AdminNavBar/>
-            <AddHotelNavBar />
-            
-            <br></br>
-            <br></br>
+        <div className="containerss">
+            <form action="http://localhost:5000/package/newPackage" method="post" class="form img responsive" encType="multipart/form-data">
 
-            <div className="containerss">
-                <form action="http://localhost:5000/hotel/newHotel" method="post" class="form img responsive" encType="multipart/form-data">
+                <h5>Add Packages</h5>
+                <br />
 
-                    <h5>Add Accomodation</h5>
-                    <br />
-                    <br />
+                <div className="col-md-7 element">
+                    <label for="hotel" className="form-label1">Hotel</label>
+                    <input type="text" name="hotel" className="form-control input-field" id="hotel" required/>
+                </div>
 
-                    <div className="col-md-7 element">
-                        <label for="hotelName" className="form-label1">Hotel Name</label>
-                        <input type="text" name="hotelName" className="form-control input-field" id="hotelName" required/>
-                    </div>
+                <div className="col-md-7 element">
+                    <label for="packageName" className="form-label1">Package Name</label>
+                    <input type="text" name="packageName" className="form-control input-field" id="packageName" required/>
+                </div>
 
-                    <br />
+                <div className="col-md-7 element">
+                    <label for="Price" className="form-label1">Price</label>
+                    <input type="text" name="Price" className="form-control input-field" id="Price" required/>
+                </div>
 
-                    <div className="col-md-7 element">
-                        <label for="location" className="form-label1">Location</label>
-                        <input type="text" name="location" className="form-control input-field" id="location" required/>
-                    </div>
+                <div className="col-md-7 element">
+                    <label for="description" className="form-label1">Description</label>
+                    <input type="text" name="description" className="form-control input-field" id="description"required/>
+                </div>
 
-                    <br />
+                <div className="element">
+                    <label for="actual-btn" className="form-label1">Upload package Image</label>
+                    <input type="file" id="actual-btn" name="image" className="upload textcolor" required/>
+                </div>
 
-                    <div className="col-md-7 element">
-                        <label for="contact" className="form-label1">Contact no.</label>
-                        <input type="text" name="contact" className="form-control input-field" id="contact" required/>
-                    </div>
-                    <br />
-
-                    <div className="col-md-7 element">
-                        <label for="description" className="form-label1">Description</label>
-                        <input type="text" name="description" className="form-control input-field" id="description"required/>
-                    </div>
-                    <br />
-
-                    <div className="element">
-                        <label for="actual-btn" className="form-label1">Upload Hotel Image</label>
-                        <input type="file" id="actual-btn" name="image" className="upload textcolor" required/>
-                    </div>
-                    <br />
-
-                    <button type="submit" className="btn Addbtn">Calculate and Save</button>
-                    <buttons type="clear" className="btn Addbtn1" onClick={clearData}>Clear</buttons>
-                    <br />
-
-                </form>
-            </div>
+                <button type="submit" className="btn Addbtn">Save</button>
+                {/* <button type="submit" className="btn Addbtn">Save</button> */}
+                <buttons type="clear" className="btn Addbtn1" onClick={clearData}>Clear</buttons>
+                
+            </form>
         </div>
-
-
-
+    </div>
     )
 }
